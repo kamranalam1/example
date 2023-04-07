4478ef898a0ac9407554608b5319608cab97214d
kamranalam1 <kamranalam098@gmail.com>
2023-04-07 05:22:30 +0500

    messeage

---
+++ app.js
@@ -0,0 +1,12 @@
+let age = +prompt("Enter Your Age")
+if (age <=10) {
+    console.log("WELLCOME SIR !")
+}else{
+    console.log("YOURE NOT ALLOWED !")
+}
+
+let table = +prompt("Enter Your Table")
+for (let i = 1; i <=10; i++){
+        console.log(table + " x " + i + " = " + table*i);
+}
+
